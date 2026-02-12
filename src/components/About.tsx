9fcbdecb549897f64c4dae421a43eb6b79b6e018
Johann Chanse Sigurdsson <jcs3@hi.is>
2026-02-12 22:06:57 +0000

    About

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ export function AboutPage() {
     return (
         <Layout title="Um verkefnið">
             <h1>Verkefnið</h1>
-            <p>Þetta er sýnilausn á verkefni 2 í vef2 2026</p>
+            <p>Þetta er lausn fra Jóhann fyrir verkefni 2 í vef2 2026</p>
         </Layout>
     )
 }
